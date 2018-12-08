655d91d92b06babf60ff43deed39ddb750e87223
LMellow <mellowlee2@gmail.com>
2018-12-08 14:42:11 +0900

    Update forms.py
    freelancerform 추가

--- forms.py
+++ forms.py
@@ -17,3 +17,21 @@ class ClientForm(forms.Form):
     phone_num_0 = forms.CharField()
     phone_num_1 = forms.CharField()
     phone_num_2 = forms.CharField()
+
+class FreelancerForm(forms.Form):
+    id = forms.CharField()
+    pw = forms.CharField()
+    uname = forms.CharField()
+    phone_num_0 = forms.CharField()
+    phone_num_1 = forms.CharField()
+    phone_num_2 = forms.CharField()
+    age = forms.IntegerField()
+    exp = forms.IntegerField()
+    major = forms.CharField()
+    language = models.CharField()
+
+class ProficiencyForm(forms.Form):
+    proficiency = models.DecimalField()
+
+class PortfolioForm(forms.Form):
+    portfolio = models.FileField()
