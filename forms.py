72dc997487e58b626ad6e6f7a455c7ff39d7945b
LMellow <mellowlee2@gmail.com>
2018-12-10 10:38:57 +0900

    form 수정
    register form에 제약조건 추가(max-langth, value)
    로그인 프론트 수정

--- forms.py
+++ forms.py
@@ -1,16 +1,7 @@
 from django import forms
 from .models import *
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = Users
-        fields = ('id', 'pw', 'uname', 'phone_num', 'user_type',)
-
 class LoginForm(forms.Form):
-    id = forms.CharField()
-    pw = forms.CharField()
-
-class ClientForm(forms.Form):
     id = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -20,28 +11,49 @@ class ClientForm(forms.Form):
     pw = forms.CharField(
         widget=forms.TextInput(
             attrs={
+                "type": "password",
+                "class": "form-control"
+            })
+    )
+
+class ClientForm(forms.Form):
+    id = forms.CharField(
+        max_length=14,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            })
+    )
+    pw = forms.CharField(
+        max_length=16,
+        widget=forms.TextInput(
+            attrs={
                 "class": "form-control"
             })
     )
     uname = forms.CharField(
+        max_length=20,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     phone_num_0 = forms.CharField(
+        max_length=3,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     phone_num_1 = forms.CharField(
+        max_length=4,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     phone_num_2 = forms.CharField(
+        max_length=4,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
@@ -57,54 +69,65 @@ class MakeRequestForm(forms.Form):
 
 class FreelancerForm(forms.Form):
     id = forms.CharField(
+        max_length = 14,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     pw = forms.CharField(
+        max_length=16,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     uname = forms.CharField(
+        max_length=20,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     phone_num_0 = forms.CharField(
+        max_length=3,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     phone_num_1 = forms.CharField(
+        max_length=4,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     phone_num_2 = forms.CharField(
+        max_length=4,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     age = forms.IntegerField(
+        min_value=0,
+        max_value=100,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     exp = forms.IntegerField(
+        min_value=0,
+        max_value=100,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
             })
     )
     major = forms.CharField(
+        max_length=30,
         widget=forms.TextInput(
             attrs={
                 "class": "form-control"
